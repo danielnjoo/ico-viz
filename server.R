b2a8b15647ae21e33deaf6f87c806538aa6a921c
Daniel Njoo <danielnjoo@gmail.com>
2018-01-20 23:40:31 -0800

    first plot

--- server.R
+++ server.R
@@ -2,21 +2,46 @@ library(shiny)
 library(googlesheets)
 library(dplyr)
 library(DT)
+library(readr)
+library(ggplot2)
 
 
 data <- gs_url("https://docs.google.com/spreadsheets/d/1ZtlHeHFO0nv5D0vkR8VdghVhsMgBaGlDAZhPVR6hqDQ/") %>% 
-    gs_read() 
+    gs_read(col_names = c("NAME", "TICKER",	"WEB SITE","ICO LENGTH (DAYS)",	"TOTAL AMOUNT RAISED($$$)",	"HARD CAP",	"TIME TILL SOFT CAP WAS REACHED",	"CURRENCIES ACCEPTED",	"BOUNTY CAMPAIGN Y/N", "BOUNTY SIZE",	"FACEBOOK", "FACEBOOK FOLLOWERS",	"REDDIT",	"REDDIT FOLLOWERS",	"TELEGRAM",	"TELEGRAM FOLLOWERS",	"TWITTER",	"TWITTER FOLLOWERS",	"SLACK",	"SLACK FOLLOWERS",	"INDUSTRY",	"TEAM SIZE",	"TEAM LOCATION",	"WHITE PAPER"),
+            skip = 6) %>% as.data.frame()
 
-colnames(data) <- data[5,]
-data <- data[6:nrow(data),]
-  
+# sapply(data, typeof) # amt raised is a character
+
+data$`TOTAL AMOUNT RAISED($$$)` <- gsub('[$,.]', '', data$`TOTAL AMOUNT RAISED($$$)`) %>% 
+  as.numeric()
 
 shinyServer(function(input, output, session) {
   
   output$the_data <- renderDataTable({
   
-    datatable(data[,c(1,5,23,25)])
+    datatable(data[,c(1,5,22,23,24)])
     
   })
-  
+
+  output$plot1 <- renderPlot({
+    
+    temp <- subset(data, `TOTAL AMOUNT RAISED($$$)`> 50000000)
+    
+    if (input$length==T){
+      plot1input <- data$`ICO LENGTH (DAYS)`
+      label1input <- temp$`ICO LENGTH (DAYS)`
+      xLabel <- "ICO length (days)"
+    } else if (input$size==T){
+      plot1input <- data$`TEAM SIZE`
+      label1input <- temp$`TEAM SIZE`
+      xLabel <- "Team size"
+    } 
+    
+    data %>% ggplot(aes(plot1input, `TOTAL AMOUNT RAISED($$$)`)) + 
+      geom_point() + 
+      geom_text(data=temp, aes(x=label1input,y=`TOTAL AMOUNT RAISED($$$)`, label=NAME), vjust=0,hjust=0) +
+      ggtitle(paste("Amount Raised vs. "), xLabel) +
+      xlab(xLabel)
+    
+  })
 })
--- ui.R
+++ ui.R
@@ -4,5 +4,23 @@ library(googlesheets)
 shinyUI(
   fluidPage(
     titlePanel("ICO data"),
-    DT::dataTableOutput("the_data")
-  ))
+    
+    p('Data manually taken from various crypto-project websites and compiled @ http://bit.ly/2DuJq8c'),
+    
+    DT::dataTableOutput("the_data"),
+    
+    hr(),
+    
+    fluidRow(
+      column(3,
+             p("Choose input"),
+             checkboxInput("length", "ICO length (days)", value=T),
+             checkboxInput("size", "Team size")
+      ),
+      column(9,
+             plotOutput('plot1')
+      )
+    )
+
+  )
+)
